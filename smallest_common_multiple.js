a1d555040ce1f8de9541125ed97f98f69fa23e6d
Jeff Wu <zhenzhenshow@gmail.com>
2022-09-24 10:03:50 +0800

    working on lcm quiz

---
+++ smallest_common_multiple.js
@@ -0,0 +1,15 @@
+// evenly divisible means leaving no remainder when divided by
+function smallestCommons(arr) {
+    // get prime factors of both number
+    let [a, b] = arr;
+    
+    function getPrimeFactors(num) {
+        let primes = [];
+        let j = 2;
+        for (let i = 0; i <= Math.sqrt(num); i++) {
+            if (num % primes[0] == 0 )
+        }
+    }
+}
+
+smallestCommons([1, 5]);
