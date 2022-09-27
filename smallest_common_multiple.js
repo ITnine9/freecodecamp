6487fa42d07733fc2819b5c4618c0541d2d8bedf
Jeff Wu <zhenzhenshow@gmail.com>
2022-09-27 22:23:06 +0800

    finish method - 2 for smallest_common_multiple

--- smallest_common_multiple.js
+++ smallest_common_multiple.js
@@ -1,15 +1,49 @@
-// evenly divisible means leaving no remainder when divided by
+// method - 1
 function smallestCommons(arr) {
-    // get prime factors of both number
-    let [a, b] = arr;
-    
-    function getPrimeFactors(num) {
-        let primes = [];
-        let j = 2;
-        for (let i = 0; i <= Math.sqrt(num); i++) {
-            if (num % primes[0] == 0 )
+    // fill arr with numbers in the range
+    arr.sort( (a, b) => a - b);
+    let length = arr[arr.length - 1] - arr[0];
+    for (let i = 1; i < length; i++) {
+        arr.splice(i, 0, arr[0] + i);
+    }
+
+    function divideEvenly(arr, num) {
+        return arr.every(n => num % n === 0);
+    }
+
+    let largest = arr[arr.length - 1];
+    let multiple = largest;
+    let start = 2;
+    while (!divideEvenly(arr.slice(0, -1), multiple)) {
+        multiple = largest * start;
+        start++;
+    }
+    return multiple;
+}
+
+console.log(smallestCommons([1, 5]));
+
+// method - 2
+function smallestCommons2(arr) {
+    const [min, max] = arr.sort( (a, b) => a -b );
+    const numberDivisor = max - min + 1;
+
+    // find largest possible value for SCM
+    let upperBound = 1;
+    for (let i = min; i <=max; i++) {
+        upperBound *= i;
+    } 
+
+    for (let multiple = max; multiple <= upperBound; multiple += max) {
+        let divisorCount = 0;
+        for (let i = min; i <= max; i++) {
+            if (multiple % i == 0 ) {
+                divisorCount++;
+            }
+        }
+        if (divisorCount === numberDivisor) {
+            return multiple;
         }
     }
 }
-
-smallestCommons([1, 5]);
+console.log(smallestCommons2([1, 5]));
